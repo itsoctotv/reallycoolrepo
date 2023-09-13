dbacad7b807e9cf798c2c34e1c0057ea317ffc3a
itsoctotv <octoyt111@gmail.com>
2023-09-13 12:15:53 +0200

    a

--- docker_runcommand.sh
+++ docker_runcommand.sh
@@ -1,1 +1,3 @@
 sudo docker run -d --name gitlab-runner --restart always   -v /srv/gitlab-runner/config:/etc/gitlab-runner   -v /var/run/docker.sock:/var/run/docker.sock   gitlab/gitlab-runner:latest
+
+
