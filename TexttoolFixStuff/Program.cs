896b3ea2fc8897b235b8361e3638d32fca4c36a7
itsoctotv <octoyt111@gmail.com>
2023-09-18 09:16:44 +0200

    C# text fix tool

---
+++ TexttoolFixStuff/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharpStuff{
+    class Progam{
+        static void Main(string[] args){
+            string path = @"input.txt";
+            
+                string[] lines = File.ReadAllLines(path);
+                for(int i = 0; i<lines.Length; i++){
+                    if(lines[i].Contains("-->")){
+                        lines[i] = lines[i].Replace("-->","");
+                    }
+                }
+                File.WriteAllLines(@"output.txt",lines);
+            
+        }
+    }
+}
